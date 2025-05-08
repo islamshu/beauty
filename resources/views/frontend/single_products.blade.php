c52ae9950a30620eb64ea339bf3bf44019b6516d
islamshu <islamshu12@gmail.com>
2025-05-08 16:30:00 +0300

    Edit

--- resources/views/frontend/single_products.blade.php
+++ resources/views/frontend/single_products.blade.php
@@ -175,13 +175,13 @@ img.lazyestload {
                         </div>
 
                         <!-- مشاركة المنتج على وسائل التواصل -->
-                        <ul class="list-inline shareSocial mt-3">
+                        {{-- <ul class="list-inline shareSocial mt-3">
                             <li>شارك المنتج:</li>
                             <li><a href="https://www.facebook.com/sharer/sharer.php?u={{ urlencode(url()->current()) }}" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
                             <li><a href="https://twitter.com/intent/tweet?url={{ urlencode(url()->current()) }}&text={{ urlencode($shareText ?? 'Check this out!') }}" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
                             <li><a href="https://pinterest.com/pin/create/button/?url={{ urlencode(url()->current()) }}&media={{ urlencode($imageUrl ?? '') }}&description={{ urlencode($shareText ?? '') }}" target="_blank"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
                             <li><a href="https://www.linkedin.com/shareArticle?mini=true&url={{ urlencode(url()->current()) }}&title={{ urlencode($title ?? '') }}&summary={{ urlencode($shareText ?? '') }}" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-                        </ul>
+                        </ul> --}}
                     </div>
                 </div>
             </div>
